e98a315761af04b3cbdb659216c181617d60cc50
Zhichao Link <sunzhichao@minijoy.work>
2024-01-24 18:02:14 +0800

    fix typo

--- Plugins/ParticleSDK/Source/ParticleSDK/Private/ParticleSDKTypes.cpp
+++ Plugins/ParticleSDK/Source/ParticleSDK/Private/ParticleSDKTypes.cpp
@@ -4,9 +4,9 @@
 // Evm
 const FString FSignMethod::Eth_SendTransaction = TEXT("eth_sendTransaction");
 const FString FSignMethod::Eth_SignTypedData = TEXT("eth_signTypedData");
-const FString FSignMethod::Eth_SignTypedData1 = TEXT("eth_signTypedData_v1");
-const FString FSignMethod::Eth_SignTypedData3 = TEXT("eth_signTypedData_v3");
-const FString FSignMethod::Eth_SignTypedData4 = TEXT("eth_signTypedData_v4");
+const FString FSignMethod::Eth_signTypedData_v1 = TEXT("eth_signTypedData_v1");
+const FString FSignMethod::Eth_SignTypedData_v3 = TEXT("eth_signTypedData_v3");
+const FString FSignMethod::Eth_SignTypedData_v4 = TEXT("eth_signTypedData_v4");
 const FString FSignMethod::Personal_Sign = TEXT("personal_sign");
 
 // Solana
